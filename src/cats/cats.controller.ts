c117bd14a6c120db2b87c60460b18b5f38b42df0
Hwan <qudghks1218@gmail.com>
2021-12-06 16:55:59 +0900

    nest providers

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,27 +1,41 @@
 import { Controller, Get, Param, Req, Post, Body } from '@nestjs/common';
 import { Request } from 'express';
-import { createCatDto } from './create-cat.dto';
+import { CreateCatDto } from './create-cat.dto';
+import { CatsService } from './cats.service';
+import {Cat} from "../interfaces/cat.interface";
 
 @Controller('cats')
 export class CatsController {
-  @Get(':id')
-  findOne(@Param() params): string {
-    // console.log('watch');
-    return `This actions returns a #${params.id} cat`;
-  }
-
-  // @Post()
-  // create(): string {
-  //   return 'this actions adds a new cat';
-  // }
+  constructor(private catsService: CatsService) {}
 
   @Post()
-  async create(@Body() createCatDto: createCatDto) {
-    return `This actions adds a new cat `;
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
 
   @Get()
-  findAll(@Req() request: Request): string {
-    return 'This action returns all cats';
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
+
+  // @Get(':id')
+  // findOne(@Param() params): string {
+  //   // console.log('watch');
+  //   return `This actions returns a #${params.id} cat`;
+  // }
+  //
+  // // @Post()
+  // // create(): string {
+  // //   return 'this actions adds a new cat';
+  // // }
+  //
+  // @Post()
+  // async create(@Body() createCatDto: createCatDto) {
+  //   return `This actions adds a new cat `;
+  // }
+  //
+  // @Get()
+  // findAll(@Req() request: Request): string {
+  //   return 'This action returns all cats';
+  // }
 }
