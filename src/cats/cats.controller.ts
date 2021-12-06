ea2c0b030df2d00345f3ddd27950dfd25738ef11
Hwan <qudghks1218@gmail.com>
2021-12-06 14:06:12 +0900

    example commit

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Param, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log('watch');
+    return `This actions returns a #${params.id} cat`;
+  }
+
+  // @Get()
+  // ddd(): string {
+  //   return 'xxx';
+  // }
+
   @Get()
   findAll(@Req() request: Request): string {
     return 'This action returns all cats';
